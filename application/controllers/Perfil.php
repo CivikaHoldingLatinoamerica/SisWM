42b55908c8562203425b1771ed4d4151c9e33a47
Enrique Corona <enrique_cr1990@hotmail.com>
2023-10-31 14:28:41 -0600

    actualizacion por issue en el perfil

--- application/controllers/Perfil.php
+++ application/controllers/Perfil.php
@@ -47,7 +47,7 @@ class Perfil extends CI_Controller {
 		);
 		$data['usuario'] = $this->usuario;
 		$data['datos_usuario'] = $this->UsuarioModel->obtener_usuario_modificar_id($this->usuario->id_usuario);
-		//dd($data);exit;
+		//var_dump($data);exit;
 		$switch_perfil = $this->usuario->perfil;
 		switch ($switch_perfil){
 			case 'root':case 'admin':
